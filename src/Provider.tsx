d2d7cbb944ac334fb4dadd8a6315d1ec9d7c981c
daishi <daishi@axlight.com>
2020-06-07 12:18:32 +0900

    wip: update state immutably

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -95,7 +95,30 @@ const getAtomStateValue = (state: State, a: Atom<unknown>) => {
   return atomState ? atomState.value : a.default;
 };
 
-const initAtomState = (state: State, atom: Atom<unknown>) => {
+const setNewAtomValue = (
+  setState: Dispatch<SetStateAction<State>>,
+  atom: Atom<unknown>,
+  value: unknown,
+) => {
+  setState((prev) => {
+    const atomState = prev.get(atom);
+    if (!atomState) {
+      throw new Error('atom is not initialized');
+    }
+    return new Map(prev).set(atom, {
+      ...atomState,
+      extendablePromise: null,
+      value,
+    });
+  });
+};
+
+
+const initAtomState = (
+  state: State,
+  setState: Dispatch<SetStateAction<State>>,
+  atom: Atom<unknown>,
+) => {
   const nextValue = atom.get({
     get: (a: Atom<unknown>) => {
       if (a !== atom) {
@@ -110,13 +133,14 @@ const initAtomState = (state: State, atom: Atom<unknown>) => {
     const extendablePromise: ExtendablePromise<void> = {
       pending: [],
       promise: new Promise<void>((resolve, reject) => {
+        let finalValue: unknown;
         nextValue
           .then((v) => {
-            atomState.value = v;
+            finalValue = v;
           })
           .then(() => extendablePromise.pending.reduce((p, f) => p.then(f), Promise.resolve()))
           .then(() => {
-            atomState.extendablePromise = null;
+            setNewAtomValue(setState, atom, finalValue);
           })
           .then(resolve)
           .catch(reject);
@@ -139,12 +163,9 @@ const initAtomState = (state: State, atom: Atom<unknown>) => {
 
 const updateValue = (
   prevState: State,
+  setState: Dispatch<SetStateAction<State>>,
   action: UpdateAction,
-  mutate?: boolean,
-): {
-  nextState: State;
-  extendablePromise: ExtendablePromise<void> | null;
-} => {
+) => {
   const nextState = new Map(prevState);
   const getValue = (a: Atom<unknown>) => getAtomStateValue(nextState, a);
   const nextValue = typeof action.update === 'function' ? (
@@ -156,7 +177,7 @@ const updateValue = (
   if (!atomState) {
     throw new Error('atom is not initialized');
   }
-  const nextAtomState = mutate ? atomState : { ...atomState };
+  let nextAtomState: AtomState<unknown> | null = { ...atomState };
   const promises: Promise<void>[] = [];
   const setValue = (atom: WritableAtom<unknown>, value: unknown) => {
     const promise = atom.set({
@@ -164,7 +185,11 @@ const updateValue = (
       set: (a: WritableAtom<unknown>, v: unknown) => {
         addSetDependent(a, action.atom);
         if (a === action.atom) {
-          nextAtomState.value = v;
+          if (nextAtomState) {
+            nextAtomState.value = v;
+          } else {
+            setNewAtomValue(setState, action.atom, v);
+          }
         } else {
           setValue(a, v);
         }
@@ -181,9 +206,6 @@ const updateValue = (
     promise: new Promise<void>((resolve, reject) => {
       Promise.all(promises)
         .then(() => extendablePromise.pending.reduce((p, f) => p.then(f), Promise.resolve()))
-        .then(() => {
-          nextAtomState.extendablePromise = null;
-        })
         .then(resolve)
         .catch(reject);
     }),
@@ -210,11 +232,11 @@ const updateValue = (
       if (!dependentState) {
         throw new Error('atom is not initialized');
       }
-      const nextDependentState = mutate ? dependentState : { ...dependentState };
+      const nextDependentState = { ...dependentState };
       if (v instanceof Promise) {
         nextDependentState.extendablePromise = extendablePromise;
         extendablePromise.pending.push(async () => {
-          nextDependentState.value = await v;
+          setNewAtomValue(setState, dependent, await v);
         });
       } else {
         nextDependentState.value = v;
@@ -228,16 +250,20 @@ const updateValue = (
   if (!promises.length && !extendablePromise.pending.length) {
     nextAtomState.extendablePromise = null;
     allDependents.delete(action.atom);
+  } else {
+    const finalValue = nextAtomState.value;
+    extendablePromise.promise.then(() => {
+      setNewAtomValue(setState, action.atom, finalValue);
+    });
   }
   allDependents.forEach((dependent) => {
     const dependentState = nextState.get(dependent);
     if (!dependentState) {
       throw new Error('atom is not initialized');
     }
-    const nextDependentState = mutate ? dependentState : { ...dependentState };
-    nextDependentState.extendablePromise = extendablePromise;
-    nextState.set(dependent, nextDependentState);
+    nextState.set(dependent, { ...dependentState, extendablePromise });
   });
+  nextAtomState = null;
   return { nextState, extendablePromise };
 };
 
@@ -250,13 +276,13 @@ export const Provider: React.FC = ({ children }) => {
     if (action.type === 'INIT_ATOM') {
       let atomState = prevState.get(action.atom);
       if (!atomState) {
-        atomState = initAtomState(prevState, action.atom);
-        return new Map(prevState).set(action.atom, atomState);
+        atomState = initAtomState(prevState, setState, action.atom);
+      } else {
+        atomState = {
+          ...atomState,
+          used: atomState.used + 1,
+        };
       }
-      atomState = {
-        ...atomState,
-        used: atomState.used + 1,
-      };
       return new Map(prevState).set(action.atom, atomState);
     }
     if (action.type === 'DISPOSE_ATOM') {
@@ -282,13 +308,17 @@ export const Provider: React.FC = ({ children }) => {
       }
       if (atomState.extendablePromise) {
         atomState.extendablePromise.pending.push(async () => {
-          // XXX to avoid race condition, we mutate previous suspended state
-          const { extendablePromise } = updateValue(prevState, action, true);
-          await extendablePromise?.promise;
+          let promiseToWait = Promise.resolve();
+          setState((prev) => {
+            const { nextState, extendablePromise } = updateValue(prev, setState, action);
+            promiseToWait = extendablePromise.promise;
+            return nextState;
+          });
+          await promiseToWait;
         });
         return prevState;
       }
-      const { nextState } = updateValue(prevState, action);
+      const { nextState } = updateValue(prevState, setState, action);
       return nextState;
     }
     throw new Error('unexpected action type');
