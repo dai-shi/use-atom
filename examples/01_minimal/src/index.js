05e3dbbad1c5576c4384081f34680a042c223099
daishi <daishi@axlight.com>
2022-03-26 09:06:36 +0900

    update dev dependencies

--- examples/01_minimal/src/index.js
+++ examples/01_minimal/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import { Provider, atom, useAtom } from 'use-atom';
 
@@ -44,4 +44,4 @@ const App = () => (
   </Provider>
 );
 
-ReactDOM.createRoot(document.getElementById('app')).render(<App />);
+createRoot(document.getElementById('app')).render(<App />);
